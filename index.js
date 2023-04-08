8b012cae7ee1b11d9d8666a7dc5241369fc8e734
Amaan Khan <100790516+amaan-kh@users.noreply.github.com>
2023-04-08 23:01:52 +0530

    Add files via upload
    first version with basic features

---
+++ index.js
@@ -0,0 +1,78 @@
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
+const form = document.querySelector("form");
+const container = document.querySelector(".container");
+
+const tasks = localStorage.getItem("tasks") 
+  ?JSON.parse(localStorage.getItem("tasks"))
+  : [];
+  showAllTasks();
+  console.log(tasks);
+
+function showAllTasks(){
+    tasks.forEach((value,index)=>{
+    const div = document.createElement("div");
+    div.setAttribute("class","task");
+
+    const innerDiv = document.createElement("div");
+    div.append(innerDiv);
+
+    const p = document.createElement("p");
+    p.innerText = value.title;
+    innerDiv.append(p);
+
+    const span = document.createElement("span");
+    span.innerText = value.description;
+    innerDiv.append(span);
+
+    const btn = document.createElement("button");
+    btn.setAttribute("class","deleteBtn");
+    btn.innerText = '-';
+    btn.addEventListener("click",()=>{
+
+        removeTasks();
+
+        tasks.splice(index,1);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+       
+
+        showAllTasks();
+    });
+    div.append(btn);
+
+    container.append(div);
+
+
+});
+
+}
+
+function removeTasks(){
+
+    tasks.forEach(()=>{
+
+        const div = document.querySelector(".task");
+        div.remove();
+    });
+}
+
+
+
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    removeTasks();
+
+    tasks.push({
+        title : title.value,
+        description: description.value
+
+    }
+    );
+
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+
+    showAllTasks();
+});
+
